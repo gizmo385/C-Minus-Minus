6766728838024c947b768beeb13be8e88c4c75b2
Christopher Chapline <cachapline8@gmail.com>
2015-10-15 10:02:55 -0700

    453 3: Modified test script

--- ComputerScience/453/type-checker/test.sh
+++ ComputerScience/453/type-checker/test.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-make clean compile
+echo "Compiling..."
+make clean compile 2>/dev/null >/dev/null
 
-for test in $(ls 453tests/*.c); do
-    ./compile < $test 2>/dev/null >/dev/null
-    echo $test " --> " $?
+# Run the passing test code
+echo "Running tests which should compile"
+for test_file in $(ls 453tests/test*.c); do
+    ./compile < $test_file 2>/dev/null >/dev/null
+    if [ $? -ne 0 ]
+    then
+        echo $test_file ": Expected 0, got " $?
+    fi
+done
+
+# Run the error tests
+echo "Running tests which should NOT compile"
+for test_file in $(ls 453tests/err*.c); do
+    ./compile < $test_file 2>/dev/null >/dev/null
+    if [ $? -ne 1 ]
+    then
+        echo $test_file ": Expected 1, got " $?
+    fi
 done
