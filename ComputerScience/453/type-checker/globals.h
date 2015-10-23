bda46033508cd218c1ee272fad473fb6405a4561
Christopher Chapline <cachapline8@gmail.com>
2015-10-23 01:28:27 -0700

    453 3: Scope variables added to globals

--- ComputerScience/453/type-checker/globals.h
+++ ComputerScience/453/type-checker/globals.h
@@ -2,11 +2,14 @@
 #define CMM_ERRORS_H
 #include <stdbool.h>
 #include "ast.h"
+#include "symtab.h"
 
 extern int mylineno;
 extern int mycolno;
 extern bool foundError;
 
 extern Type currentFunctionReturnType;
+extern Scope *globalScope;
+extern Scope *scope;
 
 #endif
