54366e90ee398ef689906c39ab9f668a0bd8dd81
Christopher Chapline <cachapline8@gmail.com>
2015-10-24 22:09:50 -0700

    453 3: Clean up test05

--- ComputerScience/453/type-checker/type_checking_tests/test05.c
+++ ComputerScience/453/type-checker/type_checking_tests/test05.c
@@ -1,6 +1,6 @@
+/* Nested stuff */
 int returnNumber(int a);
 
-/* Nested stuff */
 void main(void) {
     if(returnNumber(returnNumber(returnNumber(1))) < returnNumber(returnNumber(1 - 3 * 4 / 7))) {
         return;
