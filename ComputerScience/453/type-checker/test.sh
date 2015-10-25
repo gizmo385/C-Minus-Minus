4bbf66a87e673e82601d7bdcf01b2cdf3a51fc66
Christopher Chapline <cachapline8@gmail.com>
2015-10-24 19:33:41 -0700

    453 3: Fix output in test.sh

--- ComputerScience/453/type-checker/test.sh
+++ ComputerScience/453/type-checker/test.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+#directory=453tests
+directory=type_checking_tests
+
 echo "Compiling..."
 make clean compile 2>/dev/null >/dev/null
 
 # Run the passing test code
 echo "Running tests which should compile"
-for test_file in $(ls 453tests/test*.c); do
+for test_file in $(ls ${directory}/test*.c); do
     ./compile < $test_file 2>/dev/null >/dev/null
-    if [ $? -ne 0 ]
+    res=$?
+    if [ $res -ne 0 ]
     then
-        echo $test_file ": Expected 0, got " $?
+        echo $test_file ": Expected 0, got " $res
     fi
 done
 
 # Run the error tests
 echo "Running tests which should NOT compile"
-for test_file in $(ls 453tests/err*.c); do
+for test_file in $(ls ${directory}/err*.c); do
     ./compile < $test_file 2>/dev/null >/dev/null
-    if [ $? -ne 1 ]
+    res=$?
+    if [ $res -ne 1 ]
     then
-        echo $test_file ": Expected 1, got " $?
+        echo $test_file ": Expected 1, got " $res
     fi
 done
