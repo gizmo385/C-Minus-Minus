5ee3a50f25264900448bc3647744170a21692d7f
Christopher Chapline <cachapline8@gmail.com>
2015-10-26 21:45:20 -0700

    453 3: Remove unnecessary function types

--- ComputerScience/453/type-checker/functions.h
+++ ComputerScience/453/type-checker/functions.h
@@ -9,18 +9,4 @@
  */
 typedef int (*ComparisonFunction)(void *, void *);
 
-/*
- * An element consumer is a function that is provided to some structure iterator and performs an
- * operation on each element within the structure. This ignores the actual internal structure of
- * whatever is being iterated over. For example, in a BST, it will perform the operation on the data
- * element within each node rather than performing an operation on the node itself.
- */
-typedef void (*ElementConsumer)(void *);
-
-/*
- * A map function takes an element, performs some modification on that element and returns the
- * modified element.
- */
-typedef void *(*MapFunction)(void *);
-
 #endif
