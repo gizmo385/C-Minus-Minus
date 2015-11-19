7e48be7ebb44d1ce4fac2ae6d6f7e3a0d24ac318
Christopher Chapline <cachapline8@gmail.com>
2015-11-18 17:36:54 -0700

    453 4.2: Add ms2 tests

---
+++ ComputerScience/453/code-gen/code_gen_tests2/test01.c
@@ -0,0 +1,17 @@
+extern void print_string(char string[]);
+extern void print_int(int n);
+
+void number_on_line(int shouldBe, int result) {
+    print_string("Result should be ");
+    print_int(shouldBe);
+    print_string(", and is: ");
+    print_int(result);
+    print_string("\n");
+}
+
+void main(void) {
+    number_on_line(12 , 5 + 7);
+    number_on_line(25, 5 * 5);
+    number_on_line(2, 7 - 5);
+    number_on_line(5, 25 / 5);
+}
