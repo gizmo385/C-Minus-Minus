219dc5a4731d63f23ff430a6ae1d7b0c659fe0af
Christopher Chapline <cachapline8@gmail.com>
2015-11-19 14:10:43 -0700

    453 4.2: Pretty-ing up the output test02

--- ComputerScience/453/code-gen/code_gen_tests2/test02.c
+++ ComputerScience/453/code-gen/code_gen_tests2/test02.c
@@ -1,16 +1,21 @@
 extern void print_string(char string[]);
+extern void print_int(int n);
 
 void main(void) {
     int x;
     x = 1;
 
+    print_string("x = ");
+    print_int(x);
+    print_string("\n");
+
     if(x == 1) {
-        print_string("Hello");
+        print_string("Hello\n");
     }
 
     if(x != 1) {
-        print_string("It is not 1!");
+        print_string("It is not 1!\n");
     } else {
-        print_string("It is 1!");
+        print_string("It is 1!\n");
     }
 }
