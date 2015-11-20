fba79ab7268d3f044ae2ae1f00697f213d1eb2e9
Christopher Chapline <cachapline8@gmail.com>
2015-11-19 23:21:37 -0700

    453 4.2: Add test for function returns

---
+++ ComputerScience/453/code-gen/code_gen_tests2/test05.c
@@ -0,0 +1,35 @@
+extern void print_string(char string[]);
+extern void print_int(int n);
+
+int identity(int n) {
+    return n;
+}
+
+int recursive_fact(int n) {
+    if(n < 2) {
+        return n;
+    } else {
+        return n * recursive_fact(n - 1);
+    }
+}
+
+void main(void) {
+    int x, n;
+    n = 5;
+    x = identity(n);
+
+    if(x == n) {
+        print_string("x == n --> CORRECT\n");
+    } else {
+        print_string("x == n --> INCORRECT\n");
+        print_string("x = ");
+        print_int(x);
+        print_string("\n");
+    }
+
+    print_string("The factorial of ");
+    print_int(n);
+    print_string(" is ");
+    print_int(recursive_fact(n));
+    print_string("\n");
+}
