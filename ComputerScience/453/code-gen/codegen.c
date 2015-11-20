5fd9b3f28be4a8e0d0118c8c25592e9fb9daf15d
Christopher Chapline <cachapline8@gmail.com>
2015-11-19 23:20:46 -0700

    453 4.2: Remove unnecessary LEAVE instruction

--- ComputerScience/453/code-gen/codegen.c
+++ ComputerScience/453/code-gen/codegen.c
@@ -8,18 +8,11 @@
 
 static void functionTAC(FunctionDeclaration *declaration) {
     // Find the function scope entry
-    char *functionName = declaration->functionName;
-    ScopeElement *func = findScopeElement(scope, functionName);
     Scope *functionScope = declaration->functionScope;
 
     // Generate code for the body of the function
     Statement *body = declaration->body;
     statementTAC(functionScope, body, declaration->code);
-
-    // Leave the function
-    TACInstruction *leave = newTAC(LEAVE, func, NULL, NULL);
-    vectorAdd(declaration->code, leave);
-    debug(E_INFO, "leave %s\n\n", functionName);
 }
 
 void generateTAC(FunctionDeclaration *declarations) {
