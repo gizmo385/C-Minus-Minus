b4c4c973cc1f5644a8196a873594eb554600d6b2
Christopher Chapline <cachapline8@gmail.com>
2015-11-20 17:41:56 -0700

    453 4.2: Beef up test 06

--- ComputerScience/453/code-gen/code_gen_tests2/test06.c
+++ ComputerScience/453/code-gen/code_gen_tests2/test06.c
@@ -1,57 +1,41 @@
 extern void print_string(char string[]);
 extern void print_int(int n);
 
-/*void print_hello(void) {*/
-    /*char hello[7];*/
-    /*hello[0] = 'h';*/
-    /*hello[1] = 'e';*/
-    /*hello[2] = 'l';*/
-    /*hello[3] = 'l';*/
-    /*hello[4] = 'o';*/
-    /*hello[5] = '\n';*/
-    /*hello[6] = '\0';*/
+void print_ascii_table(void) {
+    char start;
+    char string[2];
+    string[1] = '\0';
 
-    /*print_string(hello);*/
-/*}*/
-
-/*void print_world(void) {*/
-    /*char world[7];*/
-    /*world[0] = 'w';*/
-    /*world[1] = 'o';*/
-    /*world[2] = 'r';*/
-    /*world[3] = 'l';*/
-    /*world[4] = 'd';*/
-    /*world[5] = '\n';*/
-    /*world[6] = '\0';*/
-
-    /*print_string(world);*/
-/*}*/
-
-/*void print_numbers(int nums[], int count) {*/
-    /*int i;*/
-    /*for(i = 0; i < count; i = i + 1) {*/
-        /*print_string("The number at index ");*/
-        /*print_int(i);*/
-        /*print_string(" is: ");*/
-        /*print_int(nums[i]);*/
-        /*print_string("\n");*/
-    /*}*/
-/*}*/
-
-void main(void) {
-    int nums[10], i;
-    /*print_hello();*/
-    /*print_world();*/
-
-    for(i = 0; i < 10; i = i + 1) {
-        nums[i] = i;
-    }
-
-    for(i = 0; i < 10; i = i + 1) {
-        /*print_string("nums[");*/
-        /*print_int(i);*/
-        /*print_string("] = ");*/
-        print_int(nums[i]);
+    for(start = 'A'; start <= 'z'; start = start + 1) {
+        string[0] = start;
+        print_string(string);
+        print_string(" <--> ");
+        print_int(string[0]);
         print_string("\n");
     }
+}
+
+void print_hello_world(void) {
+    char message[14];
+    message[0] = 'H';
+    message[1] = 'e';
+    message[2] = 'l';
+    message[3] = 'l';
+    message[4] = 'o';
+    message[5] = ' ';
+    message[6] = 'W';
+    message[7] = 'o';
+    message[8] = 'r';
+    message[9] = 'l';
+    message[10] = 'd';
+    message[11] = '!';
+    message[12] = '\n';
+    message[13] = '\0';
+
+    print_string(message);
+}
+
+void main(void) {
+    print_hello_world();
+    print_ascii_table();
 }
