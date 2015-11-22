338367a35217698c6ffdaf8509335c5e230b09aa
Christopher Chapline <cachapline8@gmail.com>
2015-11-21 18:32:53 -0700

    453 4.2: Revamp of test suite for code gen

--- ComputerScience/453/code-gen/test.sh
+++ ComputerScience/453/code-gen/test.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-directory=SPIM_codegen_milestone_1_tests
+directory=code_gen_tests
+num_tests=$(ls -l ${directory} | grep -E "test\d+\.c" | wc -l)
 
-echo "Compiling..."
-make clean compile 2>/dev/null >/dev/null
+echo "Compiling compiler..."
+make debug 2>/dev/null >/dev/null
 
 # Run the passing test code
 echo "Running tests..."
-for test_num in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27; do
-    echo Running test${test_num}.c
-    ./compile < ${directory}/test${test_num}.c > test${test_num}.s
-    spim -file test${test_num}.s | tail -n +2 > test${test_num}.out
-    diff test${test_num}.out SPIM_codegen_milestone_1_tests/out${test_num}
-    rm *.s *.out
+for test_num in `seq 1 ${num_tests}`; do
+    test_file="${directory}/test${test_num}.c"
+    correct_out="${directory}/out${test_num}"
+    asm_file="${directory}/test${test_num}.s"
+    output_file="${directory}/test${test_num}.out"
+
+    echo Compiling and running ${test_file} with native C compiler...
+    gcc -w -o a.out ${test_file} ${directory}/print.c
+    ./a.out > ${correct_out}
+    rm a.out
+
+    echo Compiling ${test_file} with compiler:
+    ./compile < ${test_file} > ${asm_file}
+    spim -file ${asm_file} | tail -n +2 > ${output_file}
+    diff ${output_file} ${correct_out}
+    mv debug.log debug_logs/debug${test_num}.log
 done
