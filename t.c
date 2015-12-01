2c24e99a15e9420158f2ef5ebc2692384a11ae34
Christopher Chapline <cachapline8@gmail.com>
2015-12-01 08:46:11 -0700

    Add struct expressions to t.c

--- t.c
+++ t.c
@@ -1,19 +1,17 @@
-struct Person {
-    char name[20];
-    int age;
-};
-
-struct Empty {};
-
-struct BoxedInteger {
-    int a;
-};
+extern void print_int(int n);
+extern void print_string(char message[]);
 
 struct Point {
     int x, y;
 };
 
+struct Point p;
+
 void main(void) {
-    struct Point p;
     p.x = 5;
+    p.y = 7;
+    print_int(p.x);
+    print_string(", ");
+    print_int(p.y);
+    print_string("\n");
 }
