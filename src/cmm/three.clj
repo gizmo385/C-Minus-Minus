829b492d3b31ab2b4a9e0ecc36cfa196fccdbc56
Christopher Chapline <cachapline8@gmail.com>
2016-04-21 11:55:52 -0700

    Rename generate-tac-helper to ast->tac
    The name ast->tac is more descriptive about the translation that is actually occurring in the
    function.

--- src/cmm/three.clj
+++ src/cmm/three.clj
@@ -29,7 +29,7 @@
   (filter some? (flatten (mapcat (fn [l] (if (list? l) l (list l))) tacs))))
 
 ;;; Define TAC generation multimethod
-(defmulti generate-tac-helper
+(defmulti ast->tac
   "Generates a list of three address codes based on the AST supplied."
   (fn [ast] (or (:statement-type ast) (:node-type ast))))
 
@@ -37,8 +37,8 @@
 (defn- relational-operator-tac
   "Generates three address code to handle branching statements"
   [operator left right true-destination false-destination]
-  (let [[left-place left-code] (generate-tac-helper left)
-        [right-place right-code] (generate-tac-helper right)]
+  (let [[left-place left-code] (ast->tac left)
+        [right-place right-code] (ast->tac right)]
     (tac-list
       left-code
       right-code
@@ -72,23 +72,23 @@
                              false-destination)))
 
 ;;; TAC generation for functions
-(defmethod generate-tac-helper :function [function]
+(defmethod ast->tac :function [function]
   (debug-msg "Generating TAC for function: %s\n" (:function-name function))
   (tac-list
     (new-tac :label (:function-name function))
-    (map generate-tac-helper (:body function))))
+    (map ast->tac (:body function))))
 
 ;;; TAC generation for statements
-(defmethod generate-tac-helper :return [return]
+(defmethod ast->tac :return [return]
   (if (:value return)
-    (let [[place code] (generate-tac-helper (:value return))]
+    (let [[place code] (ast->tac (:value return))]
       (tac-list
         code
         (new-tac :retval place)
         (new-tac :return-from-func))))
   (tac-list (new-tac :return-from-func)))
 
-(defmethod generate-tac-helper :while-loop [while-loop]
+(defmethod ast->tac :while-loop [while-loop]
   (debug-msg "Generating TAC for while-loop\n")
   (let [eval-label (new-random-label)
         top-label (new-random-label)
@@ -99,14 +99,14 @@
 
       ;; The loop body
       (new-tac :label top-label)
-      (doall (mapcat generate-tac-helper (:body while-loop)))
+      (doall (mapcat ast->tac (:body while-loop)))
 
       ;; The condition check
       (new-tac :label eval-label)
       (boolean-jumping-tac (:condition while-loop) top-label after-label)
       (new-tac :label after-label))))
 
-(defmethod generate-tac-helper :for-loop [for-loop]
+(defmethod ast->tac :for-loop [for-loop]
   (debug-msg "Generating TAC for for-loop\n")
   (let [eval-label (new-random-label)
         top-label (new-random-label)
@@ -117,15 +117,15 @@
 
       ;; For loop body
       (new-tac :label top-label)
-      (doall (mapcat generate-tac-helper (:body for-loop)))
-      (generate-tac-helper (:update for-loop))
+      (doall (mapcat ast->tac (:body for-loop)))
+      (ast->tac (:update for-loop))
 
       ;; Predicate evaluation
       (new-tac :label eval-label)
       (boolean-jumping-tac (:condition for-loop) top-label after-label)
       (new-tac :label after-label))))
 
-(defmethod generate-tac-helper :conditional [conditional]
+(defmethod ast->tac :conditional [conditional]
   (let [then-label (new-random-label)
         else-label (new-random-label)
         after-label (new-random-label)]
@@ -137,38 +137,38 @@
 
       ;; Then body
       (new-tac :label then-label)
-      (doall (mapcat generate-tac-helper (:then conditional)))
+      (doall (mapcat ast->tac (:then conditional)))
 
       ;; Else body, if present
       (if (:else conditional)
         (tac-list
           (new-tac :label else-label)
-          (doall (mapcat generate-tac-helper (:else conditional)))))
+          (doall (mapcat ast->tac (:else conditional)))))
 
       ;; After loop
       (new-tac :label after-label))))
 
-(defmethod generate-tac-helper :assignment [assg]
+(defmethod ast->tac :assignment [assg]
   (if (:index assg)
-    (let [[index-place index-code] (generate-tac-helper (:index assg))
-          [value-place value-code] (generate-tac-helper (:value assg))]
+    (let [[index-place index-code] (ast->tac (:index assg))
+          [value-place value-code] (ast->tac (:value assg))]
       (debug-msg "TAC: %s[%s] = %s\n" (:id assg) index-place value-place)
       (tac-list
         value-code
         index-code
         (new-tac :index-assignment (:id assg) index-place value-place)))
-    (let [[value-place value-code] (generate-tac-helper (:value assg))]
+    (let [[value-place value-code] (ast->tac (:value assg))]
       (debug-msg "TAC: %s = %s\n" (:id assg) value-place)
       (tac-list
         value-code
         (new-tac :assignment (:id assg) value-place)))))
 
 ;;; TAC generation for expressions
-(defmethod generate-tac-helper :unary-expression [expr]
+(defmethod ast->tac :unary-expression [expr]
   (case (:operator expr)
     :minus
     (let [result (new-temporary-variable)
-          [place code] (generate-tac-helper (:operand expr))]
+          [place code] (ast->tac (:operand expr))]
       (debug-msg "TAC: %s = %s %s\n" result :unary-minus place)
       [result
        (tac-list
@@ -176,10 +176,10 @@
          (new-tac :unary-minus result place))])
     (err/raise-error! "Invalid unary operator: %s\n" (:operator expr))))
 
-(defmethod generate-tac-helper :binary-expression [expr]
+(defmethod ast->tac :binary-expression [expr]
   (let [result (new-temporary-variable)
-        [left-place left-code] (generate-tac-helper (:left-operand expr))
-        [right-place right-code] (generate-tac-helper (:right-operand expr))]
+        [left-place left-code] (ast->tac (:left-operand expr))
+        [right-place right-code] (ast->tac (:right-operand expr))]
     (debug-msg "TAC: %s %s %s\n" left-place (:operator expr) right-place)
     [result
      (tac-list
@@ -195,7 +195,7 @@
   (tac-list
     ;; Handle arguments
     (for [param (:arguments func)
-          :let [[place code] (generate-tac-helper param)]]
+          :let [[place code] (ast->tac param)]]
       (tac-list
         code
         (new-tac :param place)))
@@ -203,11 +203,11 @@
     ;; The actual function call
     (new-tac :call result (:function-name func))))
 
-(defmethod generate-tac-helper :function-call-statement
+(defmethod ast->tac :function-call-statement
   [func]
   (function-call-helper func nil))
 
-(defmethod generate-tac-helper :function-call
+(defmethod ast->tac :function-call
   [func]
   (debug-msg "Generating TAC for call to function: %s\n" (:function-name func))
   (let [result (new-temporary-variable)]
@@ -215,21 +215,21 @@
     [result (function-call-helper func result)]))
 
 ;;; TAC generation for various literal expressions
-(defmethod generate-tac-helper :id-expression [expr]
+(defmethod ast->tac :id-expression [expr]
   ;; Only need to return the place, no code necessary
   [(:id expr) nil])
 
-(defmethod generate-tac-helper :literal-expression [expr]
+(defmethod ast->tac :literal-expression [expr]
   (let [result (new-temporary-variable)]
     (debug-msg "%s = %s\n" result (:value expr))
     [result
      (tac-list (new-tac :assg-literal result (:value expr)))]))
 
 ;;; Handling invalid node-types
-(defmethod generate-tac-helper :default [ast]
+(defmethod ast->tac :default [ast]
   (err/raise-error! "Cannot parse AST node of type %s: %s\n" (:node-type ast) ast))
 
 ;;; User facing functions
 (defn generate-tac [program]
-  (mapcat generate-tac-helper program))
+  (mapcat ast->tac program))
 
